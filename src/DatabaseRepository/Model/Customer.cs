3823d41406139164b2eed2f4ff73f9fe6f1f096e
Jonas B <jonas.borneroth@gmail.com>
2018-02-08 15:29:01 +0100

    Added customer class with properties in the restAPI

---
+++ src/DatabaseRepository/Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketSystem.DatabaseRepository.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public string EMail { get; set; }
+        public double Phone { get; set; }
+
+    }
+}
